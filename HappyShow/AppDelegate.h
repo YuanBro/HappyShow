84a0747be4beb37301e9935fb20c15bb4af9decd
袁星 <Yuan@YdeMacBook-Pro.local>
2017-05-24 10:22:31 +0800

    Initial Commit

---
+++ HappyShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyShow
+//
+//  Created by Yuan on 2017/5/24.
+//  Copyright © 2017年 Yuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
